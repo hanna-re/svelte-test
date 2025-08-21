71311a6b6d24f1e41851d0499f0ddd4f9d55bd93
hanna-re <rebryna.hanna@gmail.com>
2025-08-21 22:27:14 +0200

    Add files via upload

---
+++ +page.svelte
@@ -0,0 +1,14 @@
+<script>
+  let message = "Loading...";
+
+  async function fetchMessage() {
+    const res = await fetch("https://yourusername.pythonanywhere.com/api/hello");
+    const data = await res.json();
+    message = data.message;
+  }
+
+  fetchMessage();
+</script>
+
+<h1>Flask + SvelteKit</h1>
+<p>{message}</p>
